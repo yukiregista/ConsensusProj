8530a9b450baa60717c91b87731af829a3c52dfa
yuke <s.yuki.regista@gmail.com>
2024-03-30 20:08:03 +0900

    Created MCC and tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,4 +10,3 @@ def test_read_example_data():
     t = Tree_with_support.get(path = files('Consensus.example_data').joinpath('true.tre'), schema="newick")
     print(files('Consensus.example_data').joinpath('true.tre'))
 
-
