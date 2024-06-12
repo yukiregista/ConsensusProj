253117f56a449a5e7599c9ae9f4d69781b5edff5
atsuhmd <atsushi.takeda@hamadalab.com>
2024-06-12 20:06:08 +0900

    add NEx

--- src/Consensus/__init__.py
+++ src/Consensus/__init__.py
@@ -3,7 +3,7 @@ from ._consensus import Tree_with_support, TreeList_with_support, transfer_suppo
 from ._simulate_tree import birthdeath_sampling, normalize_tree_with_lognormal
 from ._greedy import STDGreedyConsensus, SQDGreedyConsensus, SUTDGreedyConsensus
 from ._metrics import SUTD_fp_fn, STD_fp_fn, SQD_fp_fn, SBD_fp_fn
-from ._visualize import plot_example_func,get_support,get_support_from_NeXML
+from ._visualize import plot_example_func,get_support,get_support_from_NeXML,read_consensus_NeXML
 
 
 __all__ = [s for s in dir() if not s.startswith('_')]
